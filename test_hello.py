428d6a3fc578e6d8092a5a0795d83bbd978b3d15
Isa <isavalladolid691@gmail.com>
2026-08-30 12:44:23 -0600

    fixing the build

--- test_hello.py
+++ test_hello.py
@@ -1,6 +1,5 @@
 from hello import add
 
-
 def test_add():
     assert add(1, 1) == 2
 
@@ -11,6 +10,5 @@ from hello import random_hash
 def test_random_hash():
     result = random_hash()
 
-
-assert isinstance(result, str)
-assert len(result) == 64
+    assert isinstance(result, str)
+    assert len(result) == 64
